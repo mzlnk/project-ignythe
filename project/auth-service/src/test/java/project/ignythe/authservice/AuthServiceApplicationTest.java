11c0ff8fd844150b724d8e8465b9127a2a0bed23
Marcin Zielonka <dev.zielonka.marcin@gmail.com>
2022-11-06 21:03:43 +0100

    ✨  provided skeleton for auth-service and payment-service

---
+++ project/auth-service/src/test/java/project/ignythe/authservice/AuthServiceApplicationTest.java
@@ -0,0 +1,15 @@
+package project.ignythe.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+// generated sample JUnit test by Spring Initializr for verifying if spring-boot app initialize properly:
+
+@SpringBootTest
+class AuthServiceApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
